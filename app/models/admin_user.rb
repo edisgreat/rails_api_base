920c1b094421201317e27df3d626b20fb88c8ce5
Ed Burnett <ed.jburnett@gmail.com>
2019-02-08 14:11:52 -0500

    default migration

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -8,7 +8,7 @@
 #  reset_password_token   :string
 #  reset_password_sent_at :datetime
 #  remember_created_at    :datetime
-#  sign_in_count          :integer          default("0"), not null
+#  sign_in_count          :integer          default(0), not null
 #  current_sign_in_at     :datetime
 #  last_sign_in_at        :datetime
 #  current_sign_in_ip     :inet
@@ -21,6 +21,7 @@
 #  index_admin_users_on_email                 (email) UNIQUE
 #  index_admin_users_on_reset_password_token  (reset_password_token) UNIQUE
 #
+
 
 class AdminUser < ApplicationRecord
   # Include default devise modules. Others available are:
